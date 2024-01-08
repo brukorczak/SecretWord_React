227e3c53735b5d59f491d5e3e782263cfdedb3f2
bruKorczak <bkorczak45@gmail.com>
2024-01-08 11:03:37 -0300

    first commit

---
+++ src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+    carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
+    fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja", "Ameixa"],
+    corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
+    programação: ["Linguagem", "Framework", "JavaScript", "React", "Java", "TypeScript"],
+    alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo", "Frango","Macarrão"],
+  };
